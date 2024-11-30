85a01bec0cad5c4021695fc5aa7bf4723ccd7801
OLIVIA OLIVIA <6182201006@student.unpar.ac.id>
2024-11-30 08:23:25 +0700

    Init

---
+++ src/main/java/com/example/tubeskamin/TubeskaminApplication.java
@@ -0,0 +1,13 @@
+package com.example.tubeskamin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TubeskaminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TubeskaminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tubeskamin/TubeskaminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tubeskamin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TubeskaminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
